5feae4230cfd0c0f93183d0568fd61ac6c5f77f2
akga20052 <140678501+akga20052@users.noreply.github.com>
2023-09-22 17:26:50 -0400

    Updated
    I needed to remove some of my personal notes

--- ddl.sql
+++ ddl.sql
@@ -3,16 +3,16 @@ CREATE TABLE Movie (
   movie_id SERIAL PRIMARY KEY,
   movie_title VARCHAR(255),
   movie_language VARCHAR(16),
-  movie_description VARCHAR(255),  -- Fixed typo here
-  movie_rating VARCHAR(5),  -- Adjusted data type to varchar(5)
-  movie_duration_min integer,  -- Adjusted data type to integer
+  movie_description VARCHAR(255),
+  movie_rating VARCHAR(5), 
+  movie_duration_min integer, 
   movie_genre VARCHAR(200)
 );
 
 -- Create Concessions table
 CREATE TABLE Concessions (
   concession_id SERIAL PRIMARY KEY,
-  price numeric (8, 2),  -- Adjusted data type to numeric(8, 2)
+  price numeric (8, 2), 
   concession_name VARCHAR(255)
 );
 
@@ -29,8 +29,8 @@ CREATE TABLE Customer (
 CREATE TABLE Booking_Ticket (
   booking_ticket_id SERIAL PRIMARY KEY,
   ticket_number integer,
-  movie_date date,  -- Adjusted data type to date
-  movie_time time,  -- Adjusted data type to time
+  movie_date date,  
+  movie_time time, 
   reservation_id integer,
   seat_id integer,
   customer_id integer,
@@ -40,12 +40,12 @@ CREATE TABLE Booking_Ticket (
 -- Create Payment_Receipt table
 CREATE TABLE Payment_Receipt (
   payment_receipt_id SERIAL PRIMARY KEY,
-  ticket_total numeric(8, 2),  -- Adjusted data type to numeric(8, 2)
-  movie_date date,  -- Adjusted data type to date
-  movie_time time,  -- Adjusted data type to time
+  ticket_total numeric(8, 2),  
+  movie_date date,  
+  movie_time time,  
   discount_coupon_id integer,
   payment_method VARCHAR(255),
-  concession_amount numeric(8, 2),  -- Adjusted data type to numeric(8, 2)
+  concession_amount numeric(8, 2), 
   booking_ticket_id integer,
   customer_id integer,
   concession_id integer
